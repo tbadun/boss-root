f71e6a4ad821eef333de74fdfbd5a28abb57ff56
tbadun <58403128+tbadun@users.noreply.github.com>
2020-09-08 14:06:40 -0400

    minions and ideas

--- server/routes/ideas.js
+++ server/routes/ideas.js
@@ -1,41 +1,62 @@
 const express = require('express');
 const ideasRouter = express.Router();
 const {
-    createMeeting,
     getAllFromDatabase,
     getFromDatabaseById,
     addToDatabase,
     updateInstanceInDatabase,
-    deleteFromDatabasebyId,
-    deleteAllFromDatabase,
+    deleteFromDatabasebyId
 } = require('../db.js');
 
 // seed?
 // DEFAULT: '/api/ideas'
 
+const DB_NAME = 'ideas';
+
 // get all ideas
 ideasRouter.get('/', (req, res, next) => {
-
+    res.send(getAllFromDatabase(DB_NAME));
 });
 
 // create new idea
 ideasRouter.post('/', (req, res, next) => {
-
+    const result = addToDatabase(DB_NAME, req.query);
+    if (result) {
+        res.status(201).send(result);
+    } else {
+        res.status(400).send();
+    }
 });
+
 
 // get specific idea by id
 ideasRouter.get('/:ideaId', (req, res, next) => {
-
+    const result = getFromDatabaseById(DB_NAME, req.params.ideaId);
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404).send();
+    }
 });
 
 // update specific idea by id
 ideasRouter.put('/:ideaId', (req, res, next) => {
-
+    var instance = req.query;
+    instance['id'] = req.params.ideaId;
+    const result = updateInstanceInDatabase(DB_NAME, instance);
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404).send();
+    }
 });
 
 // delete specific idea by id
 ideasRouter.delete('/:ideaId', (req, res, next) => {
-
+    const result = deleteFromDatabasebyId(DB_NAME, req.params.ideaId);
+    if (result) {
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
 });
-
-export default ideasRouter;
--- server/routes/minions.js
+++ server/routes/minions.js
@@ -1,41 +1,75 @@
 const express = require('express');
 const minionsRouter = express.Router();
 const {
-    createMeeting,
     getAllFromDatabase,
     getFromDatabaseById,
     addToDatabase,
     updateInstanceInDatabase,
-    deleteFromDatabasebyId,
-    deleteAllFromDatabase,
+    deleteFromDatabasebyId
 } = require('../db.js');
 
 // seed?
 // DEFAULT: '/api/minions'
 
+const DB_NAME = 'minions';
+
 // get all minions
 minionsRouter.get('/', (req, res, next) => {
-
+    res.send(getAllFromDatabase(DB_NAME));
 });
 
 // create new minion
 minionsRouter.post('/', (req, res, next) => {
-
+    // if (typeof req.query.name === 'string' && typeof req.query.weakness === 'string' && typeof req.query.title === 'string' && !isNan(req.query.salary) && req.query.salary) {
+    //     const minion = {
+    //         name: req.query.name,
+    //         weakness: req.query.weakness,
+    //         title: req.query.title,
+    //         salary: Number(req.query.salary)
+    //     };
+    //     res.status(201).send(addToDatabase(DB_NAME, minion));
+    // } else {
+    //     res.status(400).send();
+    // }
+    const result = addToDatabase(DB_NAME, req.query);
+    if (result) {
+        res.status(201).send(result);
+    } else {
+        res.status(400).send();
+    }
 });
+
 
 // get specific minion by id
 minionsRouter.get('/:minionId', (req, res, next) => {
-
+    const result = getFromDatabaseById(DB_NAME, req.params.minionId);
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404).send();
+    }
 });
 
 // update specific minion by id
 minionsRouter.put('/:minionId', (req, res, next) => {
-
+    var instance = req.query;
+    instance['id'] = req.params.minionId;
+    const result = updateInstanceInDatabase(DB_NAME, instance);
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404).send();
+    }
 });
 
 // delete specific minion by id
 minionsRouter.delete('/:minionId', (req, res, next) => {
-
+    const result = deleteFromDatabasebyId(DB_NAME, req.params.minionId);
+    if (result) {
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
 });
 
 export default minionsRouter;
