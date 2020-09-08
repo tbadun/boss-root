ed471d85c131de3efab2e7bef39fb8f74904a346
tbadun <58403128+tbadun@users.noreply.github.com>
2020-09-08 14:13:08 -0400

    api routes complete
    meetings

--- server/routes/meetings.js
+++ server/routes/meetings.js
@@ -3,29 +3,38 @@ const meetingsRouter = express.Router();
 const {
     createMeeting,
     getAllFromDatabase,
-    getFromDatabaseById,
     addToDatabase,
-    updateInstanceInDatabase,
-    deleteFromDatabasebyId,
     deleteAllFromDatabase,
 } = require('../db.js');
 
 // seed?
 // DEFAULT: '/api/meetings'
 
-// get all meetings
-meetingsRouter.get('/', (req, res, next) => {
+const DB_NAME = 'meetings';
 
+// get all ideas
+meetingsRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase(DB_NAME));
 });
 
 // create new meeting
 meetingsRouter.post('/', (req, res, next) => {
-
+    const result = addToDatabase(DB_NAME, createMeeting());
+    if (result) {
+        res.status(201).send(result);
+    } else {
+        res.status(400).send();
+    }
 });
 
-// delete all meetings by id
+// delete all meetings
 meetingsRouter.delete('/', (req, res, next) => {
-
+    const result = deleteAllFromDatabase(DB_NAME);
+    if (result) {
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
 });
 
 export default meetingsRouter;
