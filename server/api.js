c434f1d0dbef28f390eea180b9a829cdb3a74acd
tbadun <58403128+tbadun@users.noreply.github.com>
2020-09-08 14:15:56 -0400

    connect api routes to api.js

--- server/api.js
+++ server/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const apiRouter = express.Router();
+const ideasRouter = require('./routes/ideas');
+const meetingsRouter = require('./routes/meetings');
+const minionsRouter = require('./routes/minions');
 
-
+apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
+apiRouter.use('/minions', minionsRouter);
 
 module.exports = apiRouter;
